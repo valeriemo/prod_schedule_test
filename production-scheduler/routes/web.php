dfedbde0500a78289c5ea2cf96fba920e3c2a8e8
valeriemo <v_moreau@hotmail.com>
2025-02-08 12:29:23 -0500

    feat: create structure

--- production-scheduler/routes/web.php
+++ production-scheduler/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/orders/create', [OrderController::class, 'create'])->name('orders.create');
+Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
